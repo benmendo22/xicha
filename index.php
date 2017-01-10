7ee865a789b0b87ec45582579e2a2e802ea03c45
Benito <ben.m.mendoza@gmail.com>
2017-01-10 14:53:59 -0800

    adding ga tag and using new index page

--- index.php
+++ index.php
@@ -1,1 +1,526 @@
-<?php header( 'Location: index-color.html' ) ;  ?>
+<!DOCTYPE html>
+<html lang="en-us" class="no-js">
+
+	<head>
+		<meta charset="utf-8">
+        <title>Xicha Brewing [chee-chah] [broo-ing] | Salem, Oregon</title>
+        <meta name="description" content="Located in Salem, Oregon, Xicha [chee-chah] Brewing offers an inclusive, family-friendly cultural experience by creating community around high quality, fresh Latin American food and house-brewed ales and lagers.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="xichabrewing">
+
+        <!-- ================= Favicon ================== -->
+        <!-- Standard -->
+        <link rel="shortcut icon" href="img/favicon.png">
+        <!-- Retina iPad Touch Icon-->
+        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon-retina-ipad.png">
+        <!-- Retina iPhone Touch Icon-->
+        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon-retina-iphone.png">
+        <!-- Standard iPad Touch Icon--> 
+        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon-standard-ipad.png">
+        <!-- Standard iPhone Touch Icon--> 
+        <link rel="apple-touch-icon" sizes="57x57" href="img/favicon-standard-iphone.png">
+
+        <!-- ============== Resources style ============== -->
+        <link rel="stylesheet" href="css/style-color.css" />
+
+		<!-- Modernizr runs quickly on page load to detect features -->
+		<script src="js/modernizr.custom.js"></script>
+
+		<!-- GA Snippet for tracking -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-90163107-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+	</head>
+	
+	<body>
+
+		<!-- Page preloader -->
+		<div id="loading">
+			<div id="preloader">
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+
+		<!-- Overlay and Star effect -->
+		<div class="global-overlay">
+			<div class="overlay skew-part">
+
+				<!-- <div id='stars'></div>
+				<div id='stars2'></div>
+				<div id='stars3'></div> -->
+
+			</div>
+		</div>
+
+		<!-- START - Home/Left Part -->
+		<section id="left-side">
+
+			<!-- Your logo -->
+			<img src="img/logo.png" alt="" class="brand-logo" />
+
+			<div class="content">
+
+				<h1 class="text-intro opacity-0">We're working<br>
+					on something<br>
+					different.</h1>
+
+				<h2 class="text-intro opacity-0">We can’t wait to share it with you. In the mean time, follow our journey on social media and sign up to hear from us.</h2>
+
+				<nav>
+					<ul>
+						<li>
+							<a href="#" id="open-more-info" data-target="right-side" class="light-btn text-intro opacity-0">More Information</a>
+						</li>
+						<li>
+							<a data-dialog="somedialog" class="action-btn trigger text-intro opacity-0">Notify Me!</a>
+						</li>
+					</ul>
+				</nav>
+
+			</div>
+
+			<!-- Social icons -->
+			<div class="social-icons">
+
+				<a href="https://www.facebook.com/xichabrewing"><i class="fa fa-facebook"></i></a>
+				<a href="https://www.instagram.com/xicha.brewing"><i class="fa fa-instagram"></i></a>
+				<a href="https://www.twitter.com/xichabrewing"><i class="fa fa-twitter"></i></a>
+
+			</div>
+
+		</section>
+		<div id="xicha-x"></div>
+		<!-- END - Home/Left Part -->
+
+		<!-- START - More Informations/Right Part -->
+		<section id="right-side" class="hide-right">
+
+			<div class="content">
+
+				<h3>Cerveza. Comida. Cultura.</h3>
+
+				<p>At Xicha Brewing [chee-chah] [broo-ing], we offer an inclusive, family-friendly cultural experience by creating community around high quality, fresh Latin American food and house-brewed European ales and lagers. Our goal is honor Latin roots with a foundation of tradition and layers of creativity. 
+				</p>
+
+				<br><br>
+
+				<div class="photo-line">
+					
+					<div class="my-gallery row" itemscope itemtype="http://schema.org/ImageGallery">
+
+						<!-- Item -->
+						<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject" class="col-xs-12 col-sm-6 col-lg-6 clear-second">
+							
+							<!-- Link to the picture and to open the gallery / Fill up well the data-size property -->
+							<a class="box-picture" href="img/gallery-1.jpg" itemprop="contentUrl" data-size="1920x1280">
+								
+								<!-- Your picture -->
+								<img src="img/gallery-1.jpg" itemprop="thumbnail" alt="This is my work" class="img-responsive" />
+								<p>Click</p>
+								
+							</a>
+								
+							<!-- Picture's description below this one -->
+							<figcaption itemprop="caption description">
+
+								<div class="photo-details">
+								
+									<h4>Cerveza [ser-ve-sah]</h4>
+
+									<span class="border"></span>
+
+									<p>Being based in the Pacific Northwest, we recognize the power of ales and intend to accomodate the hop-driven palate. However, we strive to honor the culture of the Americas with lager-based beers.</p>
+
+								</div>
+
+							</figcaption>
+
+						</figure>
+
+						<!-- Item -->
+						<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject" class="col-xs-12 col-sm-6 col-lg-6">
+							
+							<!-- Link to the picture and to open the gallery / Fill up well the data-size property -->
+							<a class="box-picture" href="img/gallery-2.jpg" itemprop="contentUrl" data-size="1920x1280">
+								
+								<!-- Your picture -->
+								<img src="img/gallery-2.jpg" itemprop="thumbnail" alt="This is my work" class="img-responsive" />
+								<p>Click</p>
+								
+							</a>
+								
+							<!-- Picture's description below this one -->
+							<figcaption itemprop="caption description">
+
+								<div class="photo-details">
+								
+									<h4>Comida [koh-mee-dah]</h4>
+
+									<span class="border"></span>
+
+									<p>As part of our beer crafting process, we believe it is important to think about the food it can be paired with. Our aim is make our beers pleasant and drinkable, while at the same time considering flavors that link to the many Latin American dishes served at our brewery.</p>
+
+								</div>
+
+							</figcaption>
+
+						</figure>
+
+						<!-- Item -->
+						<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject" class="col-xs-12 col-sm-6 col-lg-6 clear-second">
+							
+							<!-- Link to the picture and to open the gallery / Fill up well the data-size property -->
+							<a class="box-picture" href="img/gallery-3.jpg" itemprop="contentUrl" data-size="1920x1280">
+								
+								<!-- Your picture -->
+								<img src="img/gallery-3.jpg" itemprop="thumbnail" alt="This is my work" class="img-responsive" />
+								<span class="widget-angle-bottom-left">
+									<span class="icon-text">
+										<i class="icon ion-ios-star"></i>
+									</span>
+								</span>
+								<p>Click</p>
+								
+							</a>
+								
+							<!-- Picture's description below this one -->
+							<figcaption itemprop="caption description">
+
+								<div class="photo-details">
+								
+									<h4>Cultura [cool-too-rah]</h4>
+
+									<span class="border"></span>
+
+									<p>For inspiration, we draw from an entire hemisphere's worth of culture, which leads to some of our most intriguing ideas for both food and beer. We aim our beers both at craft beer drinkers who enjoy crisp flavors, and at folks seeking a cultural experience that honestly and creatively reflects a combination of tradition and modernity.</p>
+
+								</div>
+
+							</figcaption>
+
+						</figure>
+
+						<!-- Item -->
+						<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject" class="col-xs-12 col-sm-6 col-lg-6">
+							
+							<!-- Link to the picture and to open the gallery / Fill up well the data-size property -->
+							<a class="box-picture" href="img/gallery-4.jpg" itemprop="contentUrl" data-size="1920x1280">
+								
+								<!-- Your picture -->
+								<img src="img/gallery-4.jpg" itemprop="thumbnail" alt="This is my work" class="img-responsive" />
+								<span class="widget-angle-top-right">
+									<span class="icon-text">
+										<i class="icon ion-ios-star"></i>
+									</span>
+								</span>
+								<p>Click</p>
+								
+							</a>
+								
+							<!-- Picture's description below this one -->
+							<figcaption itemprop="caption description">
+
+								<div class="photo-details">
+								
+									<h4>Equipo [eh-kee-poh]</h4>
+
+									<span class="border"></span>
+
+									<p>Our team is </p>
+
+								</div>
+
+							</figcaption>
+
+						</figure>
+							
+					</div>
+
+				</div>
+
+				<span class="separator"></span>
+
+				<h3>Get in touch with us!</h3>
+				
+				<p>Wanna say hello? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                <!-- START - Contact Form -->
+            	<form id="contact-form" name="contact-form" method="POST" data-name="Contact Form">
+
+            		<div class="row">
+
+                        <!-- Full name -->
+                        <div class="col-xs-12 col-sm-6 col-lg-6">
+                            <div class="form-group">
+                                <input type="text" id="name" class="form form-control" placeholder="Your Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Name'" name="name" data-name="Name" required>
+                            </div>
+                        </div>
+
+                        <!-- E-mail -->
+                        <div class="col-xs-12 col-sm-6 col-lg-6">
+                            <div class="form-group">
+                                <input type="email" id="email" class="form form-control" placeholder="Your Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email'" name="email-address" data-name="Email Address" required>
+                            </div>
+                        </div>
+
+                        <!-- Subject -->
+                        <div class="col-xs-12 col-sm-12 col-lg-12">
+                            <div class="form-group">
+                                <input type="text" id="subject" class="form form-control" placeholder="Write the subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Write the subject'" name="subject" data-name="Subject">
+                            </div>
+                        </div>
+
+                        <!-- Message -->
+                        <div class="col-xs-12 col-sm-12 col-lg-12 no-padding">
+                            <div class="form-group">
+                                <textarea id="text-area" class="form textarea form-control" placeholder="Your message here... 20 characters Min." onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your message here... 20 characters Min.'" name="message" data-name="Text Area" required></textarea>
+                            </div>
+                        </div>
+
+                    </div>
+
+                	<!-- Button submit -->
+                	<button type="submit" id="valid-form" class="btn btn-color">Send my Message</button>
+    
+                </form>
+                <!-- END - Contact Form -->
+	            
+	            <!-- Answer for the contact form is displayed in the next div, do not remove it. -->       
+                <div id="block-answer">
+
+					<div id="answer"></div>
+
+				</div>
+
+				<div class="info-contact row">
+					<div itemscope itemtype="http://schema.org/Restaurant">
+	                    <div class="col-xs-12 col-sm-4 col-lg-4 item-map">
+	                        <div class="contact-item"><i class="icon ion-ios-telephone"></i>
+	                            <p><span itemprop="telephone">Phone: 971-209-2872</span>
+	                            </p>
+	                        </div>
+	                    </div>
+
+	                    <div class="col-xs-12 col-sm-4 col-lg-4 item-map">
+	                        <div class="contact-item"><i class="icon ion-ios-location"></i>
+	                            <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+		                            <p><span itemprop="streetAddress">576 Patterson Street NW Suite 140</span>
+		                                <br><span itemprop="addressLocality">Salem</span>, 
+		                                    <span itemprop="addressRegion">OR</span> <span itemprop="postalCode">97304</span></p>
+	                            </div>
+	                        </div>
+	                    </div>
+
+	                    <div class="col-xs-12 col-sm-4 col-lg-4 item-map">
+	                        <div class="contact-item"><i class="icon ion-ios-email"></i>
+	                            <p><a href="mailto:info@xichabrewing.com">info@xichabrewing.com</a>
+	                            </p>
+	                        </div>
+	                    </div>
+	                </div>
+                </div>
+
+			</div>
+			<!-- /. Content -->
+
+			<footer>
+
+				<p>© Xicha Brewing Co. LLC</p>
+
+			</footer>
+
+		</section>
+		<!-- START - More Informations/Right Part -->
+
+		<!-- Button Cross to close the More Informations/Right Part -->
+		<button id="close-more-info" class="hide-close"><i class="icon ion-ios-close-outline"></i></button>
+
+		<!-- START - Newsletter Popup -->
+		<div id="somedialog" class="dialog">
+
+			<div class="dialog__overlay"></div>
+					
+			<div class="dialog__content">
+						
+				<div class="dialog-inner">
+							
+					<h4>We'll keep you posted.</h4>
+							
+					<p>Being the first to know always feels great... Signing up to our newsletter gives you <strong>exclusive access to our Soft Opening!</strong></p>
+
+					<!-- Newsletter Form -->
+					<div id="subscribe">
+
+		                <form action="php/notify-me.php" id="notifyMe" method="POST">
+
+		                    <div class="form-group">
+
+		                        <div class="controls">
+		                            
+		                        	<!-- Field  -->
+		                        	<input type="text" id="mail-sub" name="email" placeholder="Click here to write your email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Click here to write your email'" class="form-control email srequiredField" />
+
+		                        	<!-- Spinner top left during the submission -->
+		                        	<i class="fa fa-spinner opacity-0"></i>
+
+		                            <!-- Button -->
+		                            <button class="btn btn-lg submit">Get notified</button>
+
+		                            <div class="clear"></div>
+
+		                        </div>
+
+		                    </div>
+
+		                </form>
+
+						<!-- Answer for the newsletter form is displayed in the next div, do not remove it. -->
+						<div class="block-message">
+
+							<div class="message">
+
+								<p class="notify-valid"></p>
+
+							</div>
+
+						</div>
+
+        			</div>
+        			<!-- /. Newsletter Form -->
+
+				</div>
+				<!-- /. dialog-inner -->
+
+				<!-- Button Cross to close the Newsletter Popup -->
+				<button class="close-newsletter" data-dialog-close><i class="icon ion-close-round"></i></button>
+
+			</div>
+			<!-- /. dialog__content -->
+						
+		</div>
+		<!-- END - Newsletter Popup -->
+
+		<!-- Root element of PhotoSwipe, the gallery. Must have class pswp. -->
+		<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+
+		    <!-- Background of PhotoSwipe. 
+	        	It's a separate element as animating opacity is faster than rgba(). -->
+		    <div class="pswp__bg"></div>
+
+		    <!-- Slides wrapper with overflow:hidden. -->
+		    <div class="pswp__scroll-wrap">
+
+		        <!-- Container that holds slides. 
+		            PhotoSwipe keeps only 3 of them in the DOM to save memory.
+		            Don't modify these 3 pswp__item elements, data is added later on. -->
+		        <div class="pswp__container">
+		            <div class="pswp__item"></div>
+		            <div class="pswp__item"></div>
+		            <div class="pswp__item"></div>
+		        </div>
+
+		        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+		        <div class="pswp__ui pswp__ui--hidden">
+
+		            <div class="pswp__top-bar">
+
+		                <!--  Controls are self-explanatory. Order can be changed. -->
+
+		                <div class="pswp__counter"></div>
+
+		                <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+
+		                <button class="pswp__button pswp__button--share" title="Share"></button>
+
+		                <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+
+		                <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+
+		                <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
+		                <!-- element will get class pswp__preloader--active when preloader is running -->
+		                <div class="pswp__preloader">
+		                    <div class="pswp__preloader__icn">
+		                      <div class="pswp__preloader__cut">
+		                        <div class="pswp__preloader__donut"></div>
+		                      </div>
+		                    </div>
+		                </div>
+
+		            </div>
+
+		            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+		                <div class="pswp__share-tooltip"></div> 
+		            </div>
+
+		            <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
+		            </button>
+
+		            <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
+		            </button>
+
+		            <div class="pswp__caption">
+		                <div class="pswp__caption__center"></div>
+		            </div>
+
+		        </div>
+
+		    </div>
+
+		</div>
+		<!-- /. Root element of PhotoSwipe. Must have class pswp. -->
+
+	<!-- ///////////////////\\\\\\\\\\\\\\\\\\\ -->
+    <!-- ********** Resources jQuery ********** -->
+    <!-- \\\\\\\\\\\\\\\\\\\/////////////////// -->
+	
+	<!-- * Libraries jQuery, Easing and Bootstrap - Be careful to not remove them * -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.easings.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Accelerated JavaScript animation JS file -->
+	<script src="js/velocity.min.js"></script> 
+
+	<!-- Accelerated JavaScript animation UI JS file -->
+	<script src="js/velocity.ui.min.js"></script> 
+
+	<!-- Newsletter plugin -->
+	<script src="js/notifyMe.js"></script>
+
+	<!-- Contact form plugin -->
+	<script src="js/contact-me.js"></script>
+
+	<!-- Scroll plugin -->
+	<script src="js/jquery.mousewheel.js"></script>
+
+	<!-- Custom Scrollbar plugin -->
+	<script src="js/jquery.mCustomScrollbar.js"></script>
+
+	<!-- Popup Newsletter Form -->
+	<script src="js/classie.js"></script>
+	<script src="js/dialogFx.js"></script>
+
+	<!-- PhotoSwipe Core JS file -->
+	<script src="js/photoswipe.js"></script> 
+
+	<!-- PhotoSwipe UI JS file -->
+	<script src="js/photoswipe-ui-default.js"></script>
+
+	<!-- Main JS File -->
+	<script src="js/main.js"></script>
+	
+	<!--[if lt IE 10]><script type="text/javascript" src="js/placeholder.js"></script><![endif]-->
+
+	</body>
+
+</html>
